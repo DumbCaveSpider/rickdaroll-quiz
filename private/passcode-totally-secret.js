e7c4703d34ed0a3ffa83c0ca8a463924de802fbc
ArcticWoof <arcticwoofc@gmail.com>
2022-03-07 21:44:31 +0800

    making some fixes

--- private/passcode-totally-secret.js
+++ private/passcode-totally-secret.js
@@ -1,9 +1,24 @@
 function checkPswd() {
     // passcode lol
+    var check1 = "preview";
     var check1 = "rickhascringe";
     var check2 = "ShopPortalA";
     // checking passcode
     var password = document.getElementById("pswd").value;
+    if (password == preview) {
+        window.open(
+            "/quiz/1.html",
+            "toolbar=no",
+            "location=no",
+            "directories=no",
+            "status=no",
+            "menubar=no",
+            "scrollbars=no",
+            "resizable=yes",
+            "copyhistory=no"
+        );
+        return;
+    }
     if (password == check1) {
         window.open(
             "/quiz/16.html",
@@ -34,6 +49,6 @@ function checkPswd() {
     }
     // if none are correct or invaild
     else {
-        alert("That's not a vaild game code. Please try again");
+        alert("Sorry... but that's not a vaild code. Please try another code THAT WORKS!");
     }
 }

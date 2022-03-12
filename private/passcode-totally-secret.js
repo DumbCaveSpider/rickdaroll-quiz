09fc94395bf17101b2937a5287633389041db770
ArcticWoof <arcticwoofc@gmail.com>
2022-03-12 18:53:42 +0800

    done and dusted

--- private/passcode-totally-secret.js
+++ private/passcode-totally-secret.js
@@ -1,13 +1,15 @@
 function checkPswd() {
     // passcode lol
-    var preview = "preview";
+    var theend = "rickanimeway";
     var check1 = "rickhascringe";
-    var check2 = "ShopPortalA";
+    var shop = "shopcodeportal";
+    var ryanhouse = "nukingwithrick";
+    
     // checking passcode
     var password = document.getElementById("pswd").value;
-    if (password == preview) {
+    if (password == theend) {
         window.open(
-            "/quiz/1",
+            "/quiz/57",
             "toolbar=no",
             "location=no",
             "directories=no",
@@ -17,6 +19,7 @@ function checkPswd() {
             "resizable=yes",
             "copyhistory=no"
         );
+        window.location = "/remind/popupperms"
         return;
     }
     if (password == check1) {
@@ -31,9 +34,10 @@ function checkPswd() {
             "resizable=yes",
             "copyhistory=no"
         );
+        window.location = "/remind/popupperms"
         return;
     }
-    if (password == check2) {
+    if (password == shop) {
         window.open(
             "/quiz/portal/a",
             "toolbar=no",
@@ -45,8 +49,24 @@ function checkPswd() {
             "resizable=yes",
             "copyhistory=no"
         );
+        window.location = "/remind/popupperms"
         return;
     }
+if (password == ryanhouse) {
+    window.open(
+        "/quiz/portal/bomb",
+        "toolbar=no",
+        "location=no",
+        "directories=no",
+        "status=no",
+        "menubar=no",
+        "scrollbars=no",
+        "resizable=yes",
+        "copyhistory=no"
+    );
+    window.location = "/remind/popupperms"
+    return;
+}
     // if none are correct or invaild
     else {
         alert("Sorry... but that's not a vaild code. Please try another code THAT WORKS!");
---
+++ private/question-teleport.js
@@ -0,0 +1,312 @@
+// i kid you not, it is an actual spagetti code. you should take the blant on it and how disgusting it is :)
+function questionNum() {
+    var numQ;
+    // yum spagetti code
+    var q1 = "1";
+    var q2 = "2";
+    var q3 = "3";
+    var q4 = "4";
+    var q5 = "5";
+    var q6 = "6";
+    var q7 = "7";
+    var q8 = "8";
+    var q9 = "9";
+    var q10 = "10";
+    var q11 = "11";
+    var q12 = "12";
+    var q13 = "13";
+    var q14 = "14";
+    var q15 = "15";
+    var q16 = "16";
+    var q17 = "17";
+    var q18 = "18";
+    var q19 = "19";
+    var q20 = "20";
+    var q21 = "21";
+    var q22 = "22";
+    var q23 = "23";
+    var q24 = "24";
+    var q25 = "25";
+    var q26 = "26";
+    var q27 = "27";
+    var q28 = "28";
+    var q29 = "29";
+    var q30 = "30";
+    var q31 = "31";
+    var q32 = "32";
+    var q33 = "33";
+    var q34 = "34";
+    var q35 = "35";
+    var q36 = "36";
+    var q37 = "37";
+    var q38 = "38";
+    var q39 = "39";
+    var q40 = "40";
+    var q41 = "41";
+    var q42 = "42";
+    var q43 = "43";
+    var q44 = "44";
+    var q45 = "45";
+    var q46 = "46";
+    var q47 = "47";
+    var q48 = "48";
+    var q49 = "49";
+    var q50 = "50";
+    var q51 = "51";
+    var q52 = "52";
+    var q53 = "53";
+    var q54 = "54";
+    var q55 = "55";
+    var q56 = "56";
+    var q57 = "57";
+    var q58 = "58";
+    var q59 = "59";
+    var q60 = "60";
+    // another yum spagetti code
+    numQ = prompt("Enter a Question Number to teleport");
+    if (numQ == q1) {
+        window.location = "/quiz/1"
+        return;
+    }
+    if (numQ == q2) {
+        window.location = "/quiz/2"
+        return;
+    }
+    if (numQ == q3) {
+        window.location = "/quiz/3"
+        return;
+    }
+    if (numQ == q4) {
+        window.location = "/quiz/4"
+        return;
+    }
+    if (numQ == q5) {
+        window.location = "/quiz/5"
+        return;
+    }
+    if (numQ == q6) {
+        window.location = "/quiz/6"
+        return;
+    }
+    if (numQ == q7) {
+        window.location = "/quiz/7"
+        return;
+    }
+    if (numQ == q8) {
+        window.location = "/quiz/8"
+        return;
+    }
+    if (numQ == q9) {
+        window.location = "/quiz/9"
+        return;
+    }
+    if (numQ == q10) {
+        window.location = "/quiz/10"
+        return;
+    }
+    if (numQ == q11) {
+        window.location = "/quiz/11"
+        return;
+    }
+    if (numQ == q12) {
+        window.location = "/quiz/12"
+        return;
+    }
+    if (numQ == q13) {
+        window.location = "/quiz/13"
+        return;
+    }
+    if (numQ == q14) {
+        window.location = "/quiz/14"
+        return;
+    }
+    if (numQ == q15) {
+        window.location = "/quiz/15"
+        return;
+    }
+    if (numQ == q16) {
+        window.location = "/quiz/16"
+        return;
+    }
+    if (numQ == q17) {
+        window.location = "/quiz/17"
+        return;
+    }
+    if (numQ == q18) {
+        window.location = "/quiz/18"
+        return;
+    }
+    if (numQ == q19) {
+        window.location = "/quiz/19"
+        return;
+    }
+    if (numQ == q20) {
+        window.location = "/quiz/20"
+        return;
+    }
+    if (numQ == q21) {
+        window.location = "/quiz/21"
+        return;
+    }
+    if (numQ == q22) {
+        window.location = "/quiz/22"
+        return;
+    }
+    if (numQ == q23) {
+        window.location = "/quiz/23"
+        return;
+    }
+    if (numQ == q24) {
+        window.location = "/quiz/24"
+        return;
+    }
+    if (numQ == q25) {
+        window.location = "/quiz/25"
+        return;
+    }
+    if (numQ == q26) {
+        window.location = "/quiz/26"
+        return;
+    }
+    if (numQ == q27) {
+        window.location = "/quiz/27"
+        return;
+    }
+    if (numQ == q28) {
+        window.location = "/quiz/28"
+        return;
+    }
+    if (numQ == q29) {
+        window.location = "/quiz/29"
+        return;
+    }
+    if (numQ == q30) {
+        window.location = "/quiz/30"
+        return;
+    }
+    if (numQ == q31) {
+        window.location = "/quiz/31"
+        return;
+    }
+    if (numQ == q32) {
+        window.location = "/quiz/32"
+        return;
+    }
+    if (numQ == q33) {
+        window.location = "/quiz/33"
+        return;
+    }
+    if (numQ == q34) {
+        window.location = "/quiz/34"
+        return;
+    }
+    if (numQ == q35) {
+        window.location = "/quiz/35"
+        return;
+    }
+    if (numQ == q36) {
+        window.location = "/quiz/36"
+        return;
+    }
+    if (numQ == q37) {
+        window.location = "/quiz/37"
+        return;
+    }
+    if (numQ == q38) {
+        window.location = "/quiz/38"
+        return;
+    }
+    if (numQ == q39) {
+        window.location = "/quiz/39"
+        return;
+    }
+    if (numQ == q40) {
+        window.location = "/quiz/40"
+        return;
+    }
+    if (numQ == q41) {
+        window.location = "/quiz/41"
+        return;
+    }
+    if (numQ == q42) {
+        window.location = "/quiz/42"
+        return;
+    }
+    if (numQ == q43) {
+        window.location = "/quiz/43"
+        return;
+    }
+    if (numQ == q44) {
+        window.location = "/quiz/44"
+        return;
+    }
+    if (numQ == q45) {
+        window.location = "/quiz/45"
+        return;
+    }
+    if (numQ == q46) {
+        window.location = "/quiz/46"
+        return;
+    }
+    if (numQ == q47) {
+        window.location = "/quiz/47"
+        return;
+    }
+    if (numQ == q48) {
+        window.location = "/quiz/48"
+        return;
+    }
+    if (numQ == q49) {
+        window.location = "/quiz/49"
+        return;
+    }
+    if (numQ == q50) {
+        window.location = "/quiz/50"
+        return;
+    }
+    if (numQ == q51) {
+        window.location = "/quiz/51"
+        return;
+    }
+    if (numQ == q52) {
+        window.location = "/quiz/52"
+        return;
+    }
+    if (numQ == q53) {
+        window.location = "/quiz/53"
+        return;
+    }
+    if (numQ == q54) {
+        window.location = "/quiz/54"
+        return;
+    }
+    if (numQ == q55) {
+        window.location = "/quiz/55"
+        return;
+    }
+    if (numQ == q56) {
+        window.location = "/quiz/56"
+        return;
+    }
+    if (numQ == q57) {
+        window.location = "/quiz/57"
+        return;
+    }
+    if (numQ == q58) {
+        window.location = "/quiz/58"
+        return;
+    }
+    if (numQ == q59) {
+        window.location = "/quiz/59"
+        return;
+    }
+    if (numQ == q60) {
+        window.location = "/quiz/60"
+        return;
+    }
+    // invaid
+    else {
+        alert("Invaild Location.\nPlease put a number of the question you would like to go.")
+        return;
+    }
+}
